9879166d5e7c4717eb848ee0b1840a46eace12f0
Nathan <nateyl.b@gmail.com>
2023-03-27 15:39:17 -0700

    views working, script working

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (request, response) => {
-  response.send("Hello world from within a route!");
-})
+  const name = "Meow";
+  response.render("home", {
+    title: "Hi World!",
+    message: "Our first template.",
+  });})
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ const createError = require("http-errors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//use pug
+app.set("views", path.join(__dirname, "./", "views"));
+app.set("view engine", "pug");
+
 //serving files from static
-app.use(express.static(path.join(__dirname, "backend", "static")));
+app.use(express.static(path.join(__dirname, "./", "./static" )));
 
 const rootRoutes = require("./routes/root");
 const requestTime = require("./middleware/requestTime");
