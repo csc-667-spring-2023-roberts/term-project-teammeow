479348eb624010800e86734232eda7d3fd5ad433
Nathan <nateyl.b@gmail.com>
2023-04-12 00:45:34 -0700

    local db coded added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const createError = require("http-errors");
@@ -20,7 +21,7 @@ if (process.env.NODE_ENV === "development") {
     }, 100);
   });
 
-                    app.use(connectLiveReload());
+  app.use(connectLiveReload());
 }
 const PORT = process.env.PORT || 3000;
 
@@ -32,12 +33,15 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "./", "./static")));
 
 const rootRoutes = require("./routes/root");
+const testRoutes = require("./routes/test/index.js");
+
 const requestTime = require("./middleware/requestTime");
 
 //requestTime middleware
 app.use(requestTime);
 
 app.use("/", rootRoutes);
+app.use("/test", testRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
