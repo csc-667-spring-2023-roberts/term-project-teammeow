12212226486c370b6dd0ed124f73e6653f3af641
10882579 <bahodirovbotirjon@gmail.com>
2023-04-21 23:15:53 -0700

    Auth pages implemented

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -6,6 +6,25 @@ router.get("/", (request, response) => {
   response.render("home", {
     title: "Hi World!",
     message: "Our first template.",
-  });})
+  });
+});
+
+router.get("/auth/login", (req, res) => {
+  const context = {
+    title: "Login page",
+    form_submit_url: "/auth/login",
+  };
+
+  res.render("login", context);
+});
+
+router.get("/auth/register", (req, res) => {
+  const context = {
+    title: "Sign-up page",
+    form_submit_url: "/auth/register",
+  };
+
+  res.render("register", context);
+});
 
 module.exports = router;
