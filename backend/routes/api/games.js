9d656733fffd341a087f1e1b7f610d8c67aff7f9
Nathan <nateyl.b@gmail.com>
2023-05-01 19:26:41 -0700

    added correct logic for create game

--- backend/routes/api/games.js
+++ backend/routes/api/games.js
@@ -31,4 +31,27 @@ router.get("/join/:id", async (req, res) => {
   }
 });
 
+router.post("/create", async (req, res) => {
+  console.log("hit create");
+  const {
+    user: { id: userID },
+  } = req.session;
+  const io = req.app.get("io");
+  console.log(req.body);
+  const players = req.body.players;
+  const roomTitle = req.body.room_title;
+  try {
+    const game = await Games.create(userID, players, roomTitle);
+    req.session.game = game;
+    res.locals.game = game;
+    await Games.join(game.id, userID, 1);
+    console.log("game created");
+    io.emit("game:created", { gameID: game.id, createdAt: game.created_at });
+    res.redirect(`/game/${game.id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/lobby");
+  }
+});
+
 module.exports = router;
