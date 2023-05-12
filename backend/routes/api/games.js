71aef616da77e61c61d9605d5763cca26b338d73
10882579 <bahodirovbotirjon@gmail.com>
2023-05-11 22:08:09 -0700

    Game create architecture rewritten

--- backend/routes/api/games.js
+++ backend/routes/api/games.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { Games, Deck } = require("../../db");
 
 router.post("/create", async (req, res) => {
-  const io = req.app.get("io");
   const { room, players } = req.body;
   const {
     user: { id: userID },
@@ -12,11 +11,6 @@ router.post("/create", async (req, res) => {
   try {
     const game = await Games.create(userID, players, room);
     await Games.join(game.id, userID, 1);
-    await Deck.create(game.id);
-    await Deck.dealHand(game.id, userID);
-
-    const state = await Deck.getState(game.id, userID);
-    io.emit(`game:${game.id}:updated`, state);
 
     res.redirect(`/game/${game.id}`);
   } catch (err) {
@@ -30,31 +24,28 @@ router.post("/join/:id", async (req, res) => {
     user: { id: userID },
   } = req.session;
   try {
-    let state = {};
     const game = await Games.getGameByID(gameID);
     const userJoined = await Games.isJoined(gameID, userID);
 
-    if (userJoined) {
-      state = await Deck.getState(gameID, userID);
-
-      io.emit(`deal:${game.id}:${userID}`, state);
-      res.status(200).json({ message: "Success" });
-    } else {
+    if (!userJoined) {
       const { max: lastJoined } = await Games.getjoinOrder(gameID);
+
       if (game.players > lastJoined) {
         await Games.join(gameID, userID, lastJoined + 1);
-        await Deck.dealHand(gameID, userID);
-
-        state = await Deck.getState(gameID, userID);
-        io.emit(`deal:${game.id}:${userID}`, state);
-        res.status(200).json({ message: "Success" });
       } else {
         res.status(401).json({ message: "The room is full" });
+        return;
       }
     }
+
+    res.status(200).json({ message: "Success" });
   } catch (err) {
     res.redirect("/lobby");
   }
+});
+
+router.post("/start/:id", (req, res) => {
+  // TODO: IMPLEMENT THIS
 });
 
 module.exports = router;
